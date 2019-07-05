c7f4ffa697d75215dfba4daddaa3c87d8c98fe51
anatollko <anatollko91@gmail.com>
2019-07-06 00:17:37 +0300

    adds modal i.e. order summary

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import _Aux from '../../../hoc/_Aux/_Aux'
+
+
+const orderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                </li>
+            )
+        })
+
+    return (
+        <_Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue checkout</p>
+        </_Aux>
+    )
+}
+
+export default orderSummary
