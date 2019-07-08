366f7c31f47afefae040e13eaa0bb23d999545d6
Anatoliy <anatollko91@gmail.com>
2019-07-08 23:21:37 +0300

    Adds backdrop for modal

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -26,7 +26,6 @@ class BurgerBuilder extends Component {
     }
 
     updatePurchaseState (ingredients) {
-        console.log(ingredients)
         const sum = Object.keys(ingredients)
             .map(igKey => {
                 return ingredients[igKey]
@@ -37,7 +36,6 @@ class BurgerBuilder extends Component {
             
         
         this.setState({purchaseable: sum > 0})
-        console.log(this.state)
     }
 
     addIngredientHandler = (type) => {
@@ -75,6 +73,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true})
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -85,7 +87,7 @@ class BurgerBuilder extends Component {
 
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
